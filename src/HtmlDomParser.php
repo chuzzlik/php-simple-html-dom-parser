41a69e88652465d799346394e5d697701416b735
Chuzzlik <chuzzlik@gnail.com>
2023-04-15 15:37:58 +0300

    start

---
+++ src/HtmlDomParser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Chuzzlik\HtmlDomParser;
+
+require 'simplehtmldom_2_0' . DIRECTORY_SEPARATOR . 'simple_html_dom.php';
+
+class HtmlDomParser {
+	
+	/**
+	 * @return \simplehtmldom_1_5\simple_html_dom
+	 */
+	static public function file_get_html() {
+		return call_user_func_array ( '\simplehtmldom_2_0\file_get_html' , func_get_args() );
+	}
+
+	/**
+	 * get html dom from string
+	 * @return \simplehtmldom_1_5\simple_html_dom
+	 */
+	static public function str_get_html() {
+		return call_user_func_array ( '\simplehtmldom_2_0\str_get_html' , func_get_args() );
+	}
+}
